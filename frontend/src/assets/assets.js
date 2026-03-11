8f23c707f17a5bb90e084338958ce7c7ed38ff63
Henry Zheng <nothenryzheng@gmail.com>
2026-03-10 23:27:10 -0400

    Completed Doctors base page 2:09:00

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -230,7 +230,7 @@ export const doctors = [
         _id: 'doc11',
         name: 'Dr. Zoe Kelly',
         image: doc11,
-        speciality: 'Neurologist',
+        speciality: 'Gastroenterologist',
         degree: 'MBBS',
         experience: '4 Years',
         about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
